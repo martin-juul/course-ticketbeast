1e3d39c8511581763be5731af7347c4b325808a6
Martin Juul <code@juul.xyz>
2018-10-02 15:41:43 +0200

    5.5 Precomputing the Order Amount

--- app/Concert.php
+++ app/Concert.php
@@ -110,7 +110,7 @@ class Concert extends Model
      */
     public function createOrder($email, $tickets)
     {
-        return Order::forTickets($tickets, $email);
+        return Order::forTickets($tickets, $email, $tickets->sum('price'));
     }
 
     public function addTickets($quantity)
