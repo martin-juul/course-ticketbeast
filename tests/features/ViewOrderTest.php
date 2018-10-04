57c30585045164c73ffb0269f56d38e640bea297
Martin Juul <code@juul.xyz>
2018-10-04 14:09:43 +0200

    Decoupling Data from Presentation

--- tests/features/ViewOrderTest.php
+++ tests/features/ViewOrderTest.php
@@ -3,6 +3,7 @@
 use App\Concert;
 use App\Order;
 use App\Ticket;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -15,29 +16,39 @@ class ViewOrderTest extends TestCase
     public function user_can_view_their_order_confirmation()
     {
         $this->disableExceptionHandling();
-        $concert = factory(Concert::class)->create();
+        $concert = factory(Concert::class)->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('March 12, 2017 8:00pm'),
+            'ticket_price' => 4250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+        ]);
+
         $order = factory(Order::class)->create([
             'confirmation_number' => 'ORDERCONFIRMATION1234',
             'card_last_four' => '1881',
             'amount' => 8500,
+            'email' => 'john@example.com',
         ]);
 
         $ticketA = factory(Ticket::class)->create([
             'concert_id' => $concert->id,
             'order_id' => $order->id,
-            'code' => 'TICKETCODE123',
+            'code' => 'TICKETCODE123'
         ]);
 
         $ticketB = factory(Ticket::class)->create([
             'concert_id' => $concert->id,
             'order_id' => $order->id,
-            'code' => 'TICKETCODE456',
+            'code' => 'TICKETCODE456'
         ]);
 
-        $response = $this->get('/orders/ORDERCONFIRMATION1234');
-
+        $response = $this->get("/orders/ORDERCONFIRMATION1234");
         $response->assertStatus(200);
-
         $response->assertViewHas('order', function ($viewOrder) use ($order) {
             return $order->id === $viewOrder->id;
         });
@@ -47,5 +58,14 @@ class ViewOrderTest extends TestCase
         $response->assertSee('**** **** **** 1881');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+
+        $response->assertSee('2017-03-12 20:00');
     }
 }
