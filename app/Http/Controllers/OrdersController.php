1c10c3c00bc3339f8bcc923bf29228ac8a5d7d80
Martin Juul <code@juul.xyz>
2018-10-04 13:29:32 +0200

    Driving out the Endpoint

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class OrdersController extends Controller
+{
+    public function show()
+    {
+
+    }
+}
---
+++ tests/features/ViewOrderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Concert;
+use App\Order;
+use App\Ticket;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewOrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function user_can_view_their_order_confirmation()
+    {
+        $this->disableExceptionHandling();
+        $concert = factory(Concert::class)->create();
+        $order = factory(Order::class)->create([
+            'confirmation_number' => 'ORDERCONFIRMATION1234'
+        ]);
+        $ticket = factory(Ticket::class)->create([
+            'concert_id' => $concert->id,
+            'order_id' => $order->id,
+        ]);
+
+        $response = $this->get('/orders/ORDERCONFIRMATION1234');
+
+        $response->assertStatus(200);
+    }
+}
