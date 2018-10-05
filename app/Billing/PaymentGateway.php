e343c610fa0c05dc9ce8fefe2314a3b5bd0a3667
Martin Juul <code@juul.xyz>
2018-10-05 11:03:57 +0200

    Leveraging Our Contract Tests

--- app/Billing/PaymentGateway.php
+++ app/Billing/PaymentGateway.php
@@ -4,8 +4,6 @@ namespace App\Billing;
 
 interface PaymentGateway
 {
-    public const TEST_CARD_NUMBER = '';
-
     public function charge($amount, $token);
 
     public function getValidTestToken($cardNumber);
